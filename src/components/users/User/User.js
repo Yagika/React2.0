eae95f58e5f2e9132bcd8631080c3d7fbf4bba03
Yagika <kucherenkoyanna@gmail.com>
2022-04-11 16:19:18 +0200

    all

--- src/components/users/User/User.js
+++ src/components/users/User/User.js
@@ -3,7 +3,7 @@ import './User.css'
 export const User = ({user}) => {
     let {id, name, username, phone, email} = user;
     return (
-        <div className={'user'}>
+        <div className={user.id%2? 'beige user':'brown user'}>
             <h2>{id}.{name}</h2>
             <h3>{username}</h3>
             <p>{email}</p>
